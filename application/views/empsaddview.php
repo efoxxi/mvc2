7d525afa158cf1cf6fbd8cb5583b929844e02520
c6320 <efoxxi@gmail.com>
2011-06-24 05:17:23 +1000

    header/footer & titles

--- application/views/empsaddview.php
+++ application/views/empsaddview.php
@@ -1,33 +1,23 @@
-<html>
-    <head>
-        <title><?php echo $title ?></title>
-    </head>
-    <body>
-        <h1><?php echo $heading ?></h1>
+<?php include_once("header.php"); ?>
 
-        <h3>Enter new details below</h3>	
+<h3>Enter new details below</h3>	
 
+<form action="<?php echo base_url(); ?>index.php/emps/insertemp" method="post">
+    <? //or using form helper //=form_open('blog/insertentry');?>
 
-        <form action="<?php echo base_url(); ?>index.php/emps/insertemp" method="post">
-            <? //or using form helper //=form_open('blog/insertentry');?>
+    <p>Employee ID:<br />
+        <input type='text' name='strEmployeeId'></p>
+    <p>First Name:<br />
+        <input type='text' name='strEmployeeFirstName'></p>
+    <p>Last Name:<br>
+        <input type='text' name='strEmployeeLastName'></p>
+    <p>Department:<br />
+        <input type='text' name='strDepartmentName'></p>
+    <p>Salary:<br />
+        <input type='text' name='intSalary'></p>
 
-            <p>Employee ID:<br />
-                <input type='text' name='strEmployeeId'></p>
-            <p>First Name:<br />
-                <input type='text' name='strEmployeeFirstName'></p>
-            <p>Last Name:<br>
-                <input type='text' name='strEmployeeLastName'></p>
-            <p>Department:<br />
-                <input type='text' name='strDepartmentName'></p>
-            <p>Salary:<br />
-                <input type='text' name='intSalary'></p>
+    <p><input type='submit' value='Submit'></p>
 
-            <p><input type='submit' value='Submit'></p>
+</form>
 
-        </form>
-
-
-
-
-    </body>
-</html>
+<?php include_once("footer.php"); ?>
