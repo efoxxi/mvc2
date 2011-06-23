24b0140440aff5ae96d4a743d102278f56d8777a
c6320 <efoxxi@gmail.com>
2011-06-24 03:16:00 +1000

    location fixes

--- application/views/empsaddview.php
+++ application/views/empsaddview.php
@@ -8,29 +8,26 @@
         <h3>Enter new details below</h3>	
 
 
-        <?
-//form action="index.php/emps/insertemp" method="post">
-//or using form helper //
-        echo form_open('emps/insertemp');
-        ?>
+        <form action="http://localhost/mvc/index.php/emps/insertemp" method="post">
+            <? //or using form helper //=form_open('blog/insertentry');?>
 
-        <p>Employee ID:<br>
-            <input type='text' name='strEmployeeId'></p>
-        <p>First Name:<br>
-            <input type='text' name='strEmployeeFirstName'></p>
-        <p>Last Name:<br>
-            <input type='text' name='strEmployeeLastName'></p>
-        <p>Department:<br>
-            <input type='text' name='strDepartmentName'></p>
-        <p>Salary:<br>
-            <input type='text' name='intSalary'></p>
+            <p>Employee ID:<br>
+                <input type='text' name='strEmployeeId'></p>
+            <p>First Name:<br>
+                <input type='text' name='strEmployeeFirstName'></p>
+            <p>Last Name:<br>
+                <input type='text' name='strEmployeeLastName'></p>
+            <p>Department:<br>
+                <input type='text' name='strDepartmentName'></p>
+            <p>Salary:<br>
+                <input type='text' name='intSalary'></p>
 
-        <p><input type='submit' value='Submit'></p>
+            <p><input type='submit' value='Submit'></p>
 
-    </form>
+        </form>
 
 
 
 
-</body>
+    </body>
 </html>
--- application/views/empsview.php
+++ application/views/empsview.php
@@ -5,9 +5,7 @@
     <body>
         <p><?= $heading ?></p>
 
-
-        <a href = "index.php/emps/addemp">Add Employee</a>
-
+        <?php echo anchor('emps/addemp', 'Add Employee'); ?>
         <table width="57%" border="1" cellspacing="0" cellpadding="0" align="center">
             <tr>
                 <th>Employee ID</th>
@@ -20,10 +18,10 @@
             <?php foreach ($res as $row): ?>
                 <tr>
                     <td><?php echo $row->strEmployeeId; ?></td>
-                    <td ><?php echo $row->strEmployeeFirstName; ?></td>
-                    <td ><?php echo $row->strEmployeeLastName; ?></td>
-                    <td ><?php echo $row->strDepartmentName; ?></td>
-                    <td ><?php echo $row->intSalary ?></td>
+                    <td><?php echo $row->strEmployeeFirstName; ?></td>
+                    <td><?php echo $row->strEmployeeLastName; ?></td>
+                    <td><?php echo $row->strDepartmentName; ?></td>
+                    <td><?php echo $row->intSalary ?></td>
                     <td><?php echo anchor('emps/deleteemp/' . $row->strEmployeeId, 'Delete'); ?></td>
                 </tr>
             <?php endforeach; ?>
