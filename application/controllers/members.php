f7a389016685df86831cdfec709a21f70463bd7a
c6320 <efoxxi@gmail.com>
2011-06-25 06:53:22 +1000

    member -> item

--- application/controllers/members.php
+++ application/controllers/members.php
@@ -1,58 +1,62 @@
 <?php
 
 class Members extends CI_Controller {
-    public $items = "members";
-    public $Item = "Member";
+
+    private $Item = "Member";
+    private $items;
+    private $modelitems;
     
     function __construct() {
         parent::__construct();
-        $this->load->model('model'.$this->items);
+        $this->items = strtolower($Item)."s";
+        $this->modelitems = "model".$this->items;
+        $this->load->model($curr);
         $this->load->helper('url');
         $this->load->helper('form');
     }
 
     function index() {
-        $data['title'] = $this->Item."s application";
-        $data['heading'] = $this->Item."s list";
-        $data['res'] = $this->modelmembers->get_all();
-        $this->load->view('view'.$this->items, $data);
+        $data['title'] = $this->Item . "s application";
+        $data['heading'] = $this->Item . "s list";
+        $data['res'] = $this->{$this->curr}->get_all();
+        $this->load->view('view' . $this->items, $data);
     }
 
     function delete() {
-        $this->modelmembers->delete();
-        redirect($this->items.'/index');
+        $this->{$this->curr}->delete();
+        redirect($this->items . '/index');
     }
 
     function add() {
-        $data['title'] = "Add ".$this->Item;
-        $data['heading'] = "Add ".$this->Item;
-        $this->load->view('add'.$this->items, $data);
+        $data['title'] = "Add " . $this->Item;
+        $data['heading'] = "Add " . $this->Item;
+        $this->load->view('add' . $this->items, $data);
     }
 
     function insert() {
         $data = $_POST;
-        $this->modelmembers->insert($data);
-        redirect($this->items.'/index');
+        $this->{$this->curr}->insert($data);
+        redirect($this->items . '/index');
     }
 
     function edit() {
-        $data['title'] = "Edit ".$this->Item;
-        $data['heading'] = "Edit ".$this->Item;
+        $data['title'] = "Edit " . $this->Item;
+        $data['heading'] = "Edit " . $this->Item;
 
         $this->db->where('id', $this->uri->segment(3));
         $data['query'] = $this->db->get($this->items);
-        $this->load->view('edit'.$this->items, $data);
+        $this->load->view('edit' . $this->items, $data);
     }
 
     function update() {
         $data = $_POST;
-        $this->modelmembers->update($data);
-        redirect($this->items.'/index');
+        $this->{$this->curr}->update($data);
+        redirect($this->items . '/index');
     }
 
     function restoredb() {
-        $this->modelmembers->restoredb();
-        redirect($this->items.'/index');
+        $this->{$this->curr}->restoredb();
+        redirect($this->items . '/index');
     }
 
 }
